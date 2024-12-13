cf5ead8a431611339a5b38ec60992ede049eb046
nihaltizgha <niihaal22@gmail.com>
2024-12-13 19:03:10 +0100

    update

---
+++ src/main/java/ma/projet/grpc/services/CompteService.java
@@ -0,0 +1,38 @@
+package ma.projet.grpc.services;
+
+import ma.projet.grpc.entities.Compte;
+import ma.projet.grpc.repositories.CompteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CompteService {
+
+    private final CompteRepository compteRepository;
+
+    public CompteService(CompteRepository compteRepository) {
+        this.compteRepository = compteRepository;
+    }
+
+    public List<Compte> findAllComptes() {
+        return compteRepository.findAll();
+    }
+
+    public Compte findCompteById(int id) {
+        return compteRepository.findById(id).orElse(null);
+    }
+
+    public Compte saveCompte(Compte compte) {
+        return compteRepository.save(compte);
+    }
+
+    public boolean deleteCompteById(int id) {
+        if (compteRepository.existsById(id)) {
+            compteRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
+
+}
